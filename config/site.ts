90359154fa802756f6a6bc31145ebaefd191e93d
differenz94 <arvina.kori@differenzsystem.com>
2023-11-24 18:19:07 +0530

    logo changes

---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Taskify",
+  description:
+    " Collaborate, Manage Projects and reach new productivity peaks. From higrises to the home office, the way your team works is unique - accomplish it all with Taskify.",
+};
